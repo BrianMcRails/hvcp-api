0190b4700a0888c7ba5d8116148280fd3bad0731
BrianMcRails <brian.mcauley.rails@gmail.com>
2017-02-01 14:12:47 -0800

    added api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :demographics
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        resources :demographics
+      end
+    end
+  end
 end
